fdc32a8439fb83e8cb1117f3ff2101df9dcdf872
Khushi Chopra <khushichopra824@gmail.com>
2026-06-04 18:11:41 +0530

    Add apps/templates/views.py

---
+++ apps/templates/views.py
@@ -0,0 +1,26 @@
+from rest_framework import filters, permissions, viewsets
+from rest_framework.authentication import SessionAuthentication
+
+from apps.authentication.authentication import BearerTokenAuthentication
+
+from .models import ContentTemplate
+from .serializers import ContentTemplateSerializer
+
+
+class ContentTemplateViewSet(viewsets.ModelViewSet):
+    serializer_class = ContentTemplateSerializer
+    authentication_classes = [SessionAuthentication, BearerTokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["name", "prompt_template", "channel", "objective"]
+    ordering_fields = ["name", "channel", "objective", "created_at", "updated_at"]
+    ordering = ["name"]
+
+    def get_queryset(self):
+        queryset = ContentTemplate.objects.select_related("user")
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer: ContentTemplateSerializer) -> None:
+        serializer.save(user=self.request.user)
