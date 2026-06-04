4e6691d8f61d9ae4c1e5ccf5294e46c885307b42
Khushi Chopra <khushichopra824@gmail.com>
2026-06-04 18:11:39 +0530

    Add apps/api_urls.py

---
+++ apps/api_urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+
+urlpatterns = [
+    path("auth/", include("apps.authentication.urls")),
+    path("brands/", include("apps.brands.urls")),
+    path("templates/", include("apps.templates.urls")),
+    path("usage/", include("apps.usage.urls")),
+]
+
